1f81b9dffcf507a216449d4d8deaecf57bc59344
Shadersinc <63486369+Shadersinc@users.noreply.github.com>
2022-03-02 16:07:59 -0400

    Add files via upload

---
+++ install_kickthemout.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get install python3 nmap git &&
+git clone https://github.com/k4m4/kickthemout.git &&
+cd kickthemout/ &&
+sudo -H pip3 install -r requirements.txt &&
+sudo python3 kickthemout.py
